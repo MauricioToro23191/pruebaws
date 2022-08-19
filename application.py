1f7d42684355f93624d595ce989519fded9be6dd
Edwin Mauricio Toro Alvarez <edwin_toro23191@elpoli.edu.co>
2022-08-19 17:37:36 -0500

    gola

--- application.py
+++ application.py
@@ -3,7 +3,7 @@ application=Flask(__name__)
 
 @application.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('proto_Solvo.html')
 
 if __name__=='__main__':
     application.run(host="0.0.0.0",port=80,debug=True)
