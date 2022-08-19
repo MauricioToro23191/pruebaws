cd32f5ac6b9f34e7fe87beedb67ca9cdb2c47e46
Edwin Mauricio Toro Alvarez <edwin_toro23191@elpoli.edu.co>
2022-08-19 18:04:51 -0500

    hola

--- application.py
+++ application.py
@@ -1,9 +1,13 @@
 from flask import Flask,render_template
-application=Flask(__name__)
+from flask_cors import CORS
+from init import init_app2
+
+application,db=init_app2()
+CORS(application)
 
 @application.route('/')
 def index():
     return render_template('proto_Solvo.html')
 
 if __name__=='__main__':
-    application.run(host="0.0.0.0",port=80,debug=True)
+    application.run(host="0.0.0.0",port=5000,debug=True)
