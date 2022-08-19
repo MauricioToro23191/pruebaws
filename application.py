7aa9882fa1e5359410cc469c5178f2b7a656aa42
Edwin Mauricio Toro Alvarez <edwin_toro23191@elpoli.edu.co>
2022-08-19 16:29:29 -0500

    8

--- application.py
+++ application.py
@@ -6,4 +6,4 @@ def index():
     return render_template('index.html')
 
 if __name__=='__main__':
-    application.run(host="0.0.0.0",port=5000,debug=True)
+    application.run(host="0.0.0.0",port=80,debug=True)
