8433a3d84629ca6bea592fa2cad43a620c6b5eb1
Edwin Mauricio Toro Alvarez <edwin_toro23191@elpoli.edu.co>
2022-08-19 18:06:37 -0500

    hola

--- application.py
+++ application.py
@@ -10,4 +10,4 @@ def index():
     return render_template('proto_Solvo.html')
 
 if __name__=='__main__':
-    application.run(host="0.0.0.0",port=5000,debug=True)
+    application.run(host="0.0.0.0",port=80,debug=True)
