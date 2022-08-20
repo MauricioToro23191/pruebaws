d47008c93ed13483a37ba91fa30d80205950f381
Edwin Mauricio Toro Alvarez <edwin_toro23191@elpoli.edu.co>
2022-08-19 20:25:01 -0500

    kfkk

--- application.py
+++ application.py
@@ -1,6 +1,4 @@
-from flask import Flask,render_template,request,jsonify
-from ModelUser import ModelUser
-from entities.User import User
+from flask import Flask,render_template
 from flask_mysqldb import MySQL
 
 application=Flask(__name__)
@@ -9,19 +7,13 @@ application.secret_key='mysecretkey'
 application.config['MYSQL_HOST']='us-cdbr-east-06.cleardb.net'
 application.config['MYSQL_USER']='b1a740d25c64d3'
 application.config['MYSQL_PASSWORD']='bfe2d3e7'
-application.config['MYSQL_DB']='heroku_6d336b1af578ed7'
+application.config['MYSQL_DB']='solvo'
 
 db=MySQL(application)
 
 @application.route('/')
 def index():
     return render_template('index.html')
-
-@application.route('/login',methods=['GET', 'POST'])
-def Hola():
-    user=User(0, request.form['user'],None,None,request.form['pass'])
-    R=ModelUser.login(db,user)
-    return jsonify(R.__dict__)
 
 if __name__=='__main__':
     application.run(host="0.0.0.0",port=5000,debug=True)
